c20055dc34237b8db1c573c4010cc21a0721eb40
Zack Cain <zacharylcain@gmail.com>
2024-03-13 10:42:53 -0500

    changes to pass two test after the first sets of code

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 import { useState } from "react";
-// import { }
+import satData from "./components/satData";
+
 function App() {
-  return (
+  const [sat, setSat] = useState(satData);
+   const displaySats = [...new Set(satData.map((data) => 
+    data.orbitType))];
+
+
+    const filterByType = (currentType) => {
+      const displaySats = satData.filter((newSatDisplay) => {
+         return newSatDisplay.orbitType === currentType;
+      });
+      setSat(displaySats);
+   };
+
+   return (
     <div>
       <Banner />
       <Buttons />
+      filterByType={filterByType}
+      setSat={setSat}
+      displaySats={displaySats}
       <Table />
     </div>
   );
